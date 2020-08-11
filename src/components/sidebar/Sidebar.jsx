355fd368fee0b4ccaeebf08c29ff6da5d182ba02
Diegoher <diegoherr92@gmail.com>
2020-08-11 18:07:20 -0500

    finishing mobile sidebar styles and functionality

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,80 +3,70 @@ import "./Sidebar.css";
 import once from "../../assets/once.jpeg";
 
 export default function Sidebar() {
-  const getOffsetTop = section => {
+  const getOffsetTop = (section) => {
     const top = document.querySelector(`#${section}`).offsetTop;
-    window.scrollTo({ top, behavior: 'smooth'});
+    window.scrollTo({ top, behavior: "smooth" });
   };
   return (
-    <>
-      <div className="row no-gutters">
-        <div className="col-12 picture">
-          <img src={once} alt="diegoher" />
-        </div>
+    <div className="sidebar-container">
+      <div className="picture">
+        <img src={once} alt="diegoher" />
       </div>
-      <div className="row no-gutters">
-        <div className="col-12 description">
-          <h3>Diego Herrera</h3>
-          <h4>Frontend Developer</h4>
-        </div>
+      <div className="description">
+        <h3>Diego Herrera</h3>
+        <h4>Frontend Developer</h4>
       </div>
-      <div className="row no-gutters">
-        <div className="col-12 menu">
-          <nav className="navigation">
-            <ul>
-              <li className="">
-                <button onClick={() => getOffsetTop('home')}>Home</button>
-              </li>
-              <li>
-                <button onClick={() => getOffsetTop('about')}>About</button>
-              </li>
-              <li>
-                <button onClick={() => getOffsetTop('experience')}>Experience</button>
-              </li>
-              <li>
-                <button onClick={() => getOffsetTop('education')}>Education</button>
-              </li>
-              <li>
-                <button onClick={() => getOffsetTop('skills')}>Skills</button>
-              </li>
-            </ul>
-          </nav>
-        </div>
+      <nav className="navigation">
+        <ul>
+          <li className="">
+            <button onClick={() => getOffsetTop("home")}>Home</button>
+          </li>
+          <li>
+            <button onClick={() => getOffsetTop("about")}>About</button>
+          </li>
+          <li>
+            <button onClick={() => getOffsetTop("experience")}>
+              Experience
+            </button>
+          </li>
+          <li>
+            <button onClick={() => getOffsetTop("education")}>Education</button>
+          </li>
+          <li>
+            <button onClick={() => getOffsetTop("skills")}>Skills</button>
+          </li>
+        </ul>
+      </nav>
+      <div className="icons">
+        <a href="https://github.com/Dekoher">
+          <i className="fab fa-github-square"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/diegoher-dev/">
+          <i className="fab fa-linkedin"></i>
+        </a>
+        <a href="https://twitter.com/Diegoher_dev">
+          <i className="fab fa-twitter-square"></i>
+        </a>
+        <a href="https://dev.to/diegoher">
+          <i className="fab fa-dev" title="diegoher's DEV Profile"></i>
+        </a>
+        <a href="https://diegoher.dev">
+          <i className="fas fa-globe-americas"></i>
+        </a>
       </div>
-      <div className="row no-gutters">
-        <div className="col-12 icons">
-          <a href="https://github.com/Dekoher">
-            <i className="fab fa-github-square"></i>
-          </a>
-          <a href="https://www.linkedin.com/in/diegoher-dev/">
-            <i className="fab fa-linkedin"></i>
-          </a>
-          <a href="https://twitter.com/Diegoher_dev">
-            <i className="fab fa-twitter-square"></i>
-          </a>
-          <a href="https://dev.to/diegoher">
-            <i className="fab fa-dev" title="diegoher's DEV Profile"></i>
-          </a>
-          <a href="https://diegoher.dev">
-            <i className="fas fa-globe-americas"></i>
-          </a>
-        </div>
+      <div className="footer">
+        <p className="">
+          ©{new Date().getFullYear()} Made with
+          <span role="img" aria-label="icon">
+            ❤️
+          </span>
+          and
+          <span role="img" aria-label="icon">
+            🍺
+          </span>
+          by diegoher.dev
+        </p>
       </div>
-      <div className="row no-gutters">
-        <div className="col-12 footer pt-4 pr-4 pl-4 position-relative">
-          <p className="pb-0">
-            ©Copyright ©2020 All rights reserved | This template is made with{" "}
-            <span role="img" aria-label="icon">
-              ❤️
-            </span>{" "}
-            and{" "}
-            <span role="img" aria-label="icon">
-              🍺
-            </span>{" "}
-            by diegoher.dev
-          </p>
-        </div>
-      </div>
-    </>
+    </div>
   );
 }
--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,19 +1,32 @@
 import React  from "react";
 import './Layout.css'
 import Sidebar from "../../components/sidebar/Sidebar";
-import Home from "../../components/home/Home";
-import About from "../../components/about/About";
-import Experience from "../../components/experience/Experience";
-import Education from "../../components/education/Education";
-import Skills from "../../components/skills/Skills";
+// import Home from "../../components/home/Home";
+// import About from "../../components/about/About";
+// import Experience from "../../components/experience/Experience";
+// import Education from "../../components/education/Education";
+// import Skills from "../../components/skills/Skills";
 import Scroll from "../../components/scrollComponent/Scroll";
 
 export default function Layout() {
+  const openSidebar = event => {
+    const sidebar = document.querySelector('.sidebar');
+    if (sidebar.classList.contains('open')) {
+      sidebar.classList.remove('open');
+    } else {
+      sidebar.classList.add('open');
+    }
+  };
   return (
     <div className="main-container">
-      <div className='row no-gutters'>
-        <Scroll />
-        <div className="sidebar col-lg-2 position-fixed">
+      <Scroll />
+      <button className='hamburger' onClick={(e) => openSidebar(e)}><i className="fas fa-bars"></i></button>
+      <aside className='sidebar'>
+        <Sidebar />
+      </aside>
+      <div className='pages'>
+      </div>
+        {/* <div className="sidebar col-lg-2 position-fixed">
           <Sidebar />
         </div>
         <div className='col-lg-10 offset-2 pages'>
@@ -22,8 +35,7 @@ export default function Layout() {
           <Experience />
           <Education />
           <Skills />
-        </div>
-      </div>
+        </div> */}
     </div>
   );
 }
